cd2d7f2eb9b263c0d01bdad7125696b452a2a0a3
Damon Chivers <36902693+DamonLC21@users.noreply.github.com>
2020-04-30 12:15:41 -0600

    Update pirates_controller.rb

--- app/controllers/pirates_controller.rb
+++ app/controllers/pirates_controller.rb
@@ -1,14 +1,11 @@
 class PiratesController < ApplicationController
-  before_action :set_pirate, only: [:show, :update, :destroy]
 
-  # GET /pirates
   def index
     @pirates = Pirate.all
 
     render json: @pirates, include: [:ship]
   end
 
-  # GET /pirates/1
   def show
     @pirate = Pirate.find(params[:id])
 
