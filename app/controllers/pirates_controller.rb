85539b153008b92d601086659003c4a679ec83fc
jsabath <justin.sabath@aol.com>
2020-05-04 18:02:25 -0600

    1st Commit

--- app/controllers/pirates_controller.rb
+++ app/controllers/pirates_controller.rb
@@ -12,5 +12,11 @@ class PiratesController < ApplicationController
     render json: @pirate, include: [:ship]
   end
 
+  def create
+    @pirate = Pirate.create(name: params[:name], ship_id: params[:ship_id])
+    
+    redirect_to 'http://localhost:3001'
+  end
+
 
 end
